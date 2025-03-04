047970e84afb4103655c5655658f2506875845ab
Kartikey Tripathi <153363200+Kartikey2427@users.noreply.github.com>
2025-03-04 08:40:54 +0530

    Add files via upload

---
+++ 1780.java
@@ -0,0 +1,15 @@
+// 1780 - Check if numbers is a sum of powers of three
+
+class Solution {
+    public boolean checkPowersOfThree(int n) 
+    {
+        while(n>0)
+        {
+        	if(n%3==2) return false;
+        	
+        	n/=3;
+        }
+        
+		return true;      
+    }
+}
