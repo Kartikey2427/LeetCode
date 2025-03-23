9a6e69b96cca2e3d5d3e76e04c0a961c8167c241
Kartikey Tripathi <153363200+Kartikey2427@users.noreply.github.com>
2025-03-23 11:57:04 +0530

    Add files via upload

---
+++ LeetCode/indexOfFirst28.java
@@ -0,0 +1,7 @@
+// 28 - Find the index of first occurence in a string
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
