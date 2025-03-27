14287ac13e1ed85d947dee0ce69e45a0b8e35501
Kartikey Tripathi <153363200+Kartikey2427@users.noreply.github.com>
2025-03-27 12:40:41 +0530

    Add files via upload

---
+++ LeetCode/powerOfTwo231.java
@@ -0,0 +1,21 @@
+// 231 - Power of Two
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+
+        // not a power of two
+        if (n == 0) {
+            return false;
+        }
+
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+
+        if (n == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
